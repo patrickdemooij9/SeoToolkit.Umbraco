60234886dc2a5bf7bb79dc1c242ee70869bc9b4a
patrickdemooij9 <patrickdemooij98@hotmail.com>
2021-12-14 20:52:28 +0100

    Finish up ScriptManager v1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 "# uSeoToolkit.Umbraco" 
 
 Hours worked on package (from 2/12/2021 - today):
-12 hours
+12.5 hours
 
 Does not include hours before that date. Which is probably a lot
--- src/uSeoToolkit.Umbraco.ScriptManager/App_Plugins/uSeoToolkit.ScriptManager/backoffice/ScriptManager/list.controller.js
+++
@@ -1,9 +0,0 @@
-﻿(function () {
-    "use strict";
-
-    function scriptManagerListController($scope, $http, $location, listViewHelper) {
-        var vm = this;
-    }
-
-    angular.module("umbraco").controller("uSeoToolkit.ScriptManager.ListController", scriptManagerListController);
-})();
