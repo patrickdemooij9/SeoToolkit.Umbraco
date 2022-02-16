3d77820627fc340c91a59cb2cdb59e0768e9354e
patrickdemooij9 <patrickdemooij98@hotmail.com>
2022-02-16 15:19:50 +0100

    Added base package which includes all features

---
+++ src/uSeoToolkit.Umbraco.Core/Controllers/SeoToolkitLicenseTreeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Trees;
+using Umbraco.Cms.Web.BackOffice.Trees;
+using Umbraco.Cms.Web.Common.Attributes;
+
+namespace uSeoToolkit.Umbraco.Core.Controllers
+{
+    [Tree("uSeoToolkit", "License", TreeTitle = "uSeoToolkit", TreeGroup = "uSeoToolkit", SortOrder = 2)]
+    [PluginController("uSeoToolkit")]
+    public class SeoToolkitLicenseTreeController : TreeController
+    {
+        public SeoToolkitLicenseTreeController(ILocalizedTextService localizedTextService, UmbracoApiControllerTypeCollection umbracoApiControllerTypeCollection, IEventAggregator eventAggregator) : base(localizedTextService, umbracoApiControllerTypeCollection, eventAggregator)
+        {
+        }
+
+        protected override ActionResult<TreeNodeCollection> GetTreeNodes(string id, FormCollection queryStrings)
+        {
+            return new TreeNodeCollection();
+        }
+
+        protected override ActionResult<TreeNode> CreateRootNode(FormCollection queryStrings)
+        {
+            var node = CreateTreeNode("license", "-1", queryStrings, "License", "icon-script", false,
+                $"{SectionAlias}/{TreeAlias}/licenseDashboard");
+
+            return node;
+        }
+
+        protected override ActionResult<MenuItemCollection> GetMenuForNode(string id, FormCollection queryStrings)
+        {
+            return null;
+        }
+    }
+}
---
+++ src/uSeoToolkit.Umbraco/App_Plugins/uSeoToolkit/backoffice/License/licenseDashboard.controller.js
@@ -0,0 +1,10 @@
+﻿(function() {
+    "use strict";
+
+    function licenseDashboardController($http) {
+
+        var vm = this;
+    }
+
+    angular.module("umbraco").controller("uSeoToolkit.uSeoToolkit.LicenseDashboardController", licenseDashboardController);
+})();
