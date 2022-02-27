fc8e8c5fa2cedcccc35e8c73814134f74b1b740a
leekelleher <leekelleher@gmail.com>
2022-02-27 18:40:23 +0000

    Removes 3-dot menu for tree items
    Fixes #19
    
    This also adds the "Create" menu action for ScriptManager.
    Fixes #18

--- src/uSeoToolkit.Umbraco.SiteAudit.Core/Controllers/SiteAuditTreeController.cs
+++ src/uSeoToolkit.Umbraco.SiteAudit.Core/Controllers/SiteAuditTreeController.cs
@@ -8,46 +8,53 @@ using Umbraco.Cms.Core.Trees;
 using Umbraco.Cms.Web.BackOffice.Trees;
 using Umbraco.Cms.Web.Common.Attributes;
 using Umbraco.Extensions;
+using UmbConstants = Umbraco.Cms.Core.Constants;
 
 namespace uSeoToolkit.Umbraco.SiteAudit.Core.Controllers
 {
-    [Tree("uSeoToolkit", "SiteAudit", TreeTitle = "uSeoToolkit", TreeGroup = "uSeoToolkit", SortOrder = 1)]
+    [Tree("uSeoToolkit", "SiteAudit", TreeTitle = "Site Audits", TreeGroup = "uSeoToolkit", SortOrder = 1)]
     [PluginController("uSeoToolkitSiteAudit")]
     public class SeoToolkitTreeController : TreeController
     {
-        public IMenuItemCollectionFactory MenuItemCollectionFactory { get; }
+        private readonly IMenuItemCollectionFactory _menuItemCollectionFactory;
 
         public SeoToolkitTreeController(ILocalizedTextService localizedTextService,
             UmbracoApiControllerTypeCollection umbracoApiControllerTypeCollection,
             IEventAggregator eventAggregator,
-            IMenuItemCollectionFactory menuItemCollectionFactory) : base(localizedTextService, umbracoApiControllerTypeCollection, eventAggregator)
+            IMenuItemCollectionFactory menuItemCollectionFactory)
+            : base(localizedTextService, umbracoApiControllerTypeCollection, eventAggregator)
         {
-            MenuItemCollectionFactory = menuItemCollectionFactory;
-        }
-
-        protected override ActionResult<TreeNodeCollection> GetTreeNodes(string id, FormCollection queryStrings)
-        {
-            return new TreeNodeCollection();
+            _menuItemCollectionFactory = menuItemCollectionFactory;
         }
 
         protected override ActionResult<TreeNode> CreateRootNode(FormCollection queryStrings)
         {
-            var node = CreateTreeNode("siteAudit", "-1", queryStrings, "Site Audits", "icon-diagnostics", false,
-                $"{SectionAlias}/{TreeAlias}/list");
+            var root = base.CreateRootNode(queryStrings);
 
-            return node;
+            root.Value.Icon = "icon-diagnostics";
+            root.Value.HasChildren = false;
+            root.Value.RoutePath = $"{SectionAlias}/{TreeAlias}/list";
+
+            return root.Value;
         }
 
         protected override ActionResult<MenuItemCollection> GetMenuForNode(string id, FormCollection queryStrings)
         {
-            var menuItemCollection = MenuItemCollectionFactory.Create();
-            if (id == "siteAudit")
+            if (id == UmbConstants.System.RootString)
             {
+                var menuItemCollection = _menuItemCollectionFactory.Create();
                 var item = menuItemCollection.Items.Add<ActionNew>(LocalizedTextService, opensDialog: true);
-                item.NavigateToRoute($"{queryStrings.GetRequiredValue<string>("application")}/SiteAudit/create");
+                item.NavigateToRoute($"{queryStrings.GetRequiredValue<string>("application")}/{TreeAlias}/create");
+
+                return menuItemCollection;
             }
 
-            return menuItemCollection;
+            return null;
+        }
+
+        protected override ActionResult<TreeNodeCollection> GetTreeNodes(string id, FormCollection queryStrings)
+        {
+            return null;
         }
     }
 }
