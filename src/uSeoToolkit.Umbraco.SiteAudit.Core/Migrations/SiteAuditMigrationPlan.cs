2ef5d85b072ac87e17cfe0f39bbc8a7c66e41c74
leekelleher <leekelleher@gmail.com>
2022-02-27 15:57:57 +0000

    SiteAudit - to use Package Migrations

---
+++ src/uSeoToolkit.Umbraco.SiteAudit.Core/Migrations/SiteAuditMigrationPlan.cs
@@ -0,0 +1,16 @@
+﻿using Umbraco.Cms.Core.Packaging;
+
+namespace uSeoToolkit.Umbraco.SiteAudit.Core.Migrations
+{
+    public class SiteAuditMigrationPlan : PackageMigrationPlan
+    {
+        public SiteAuditMigrationPlan()
+            : base("SEO Toolkit: Site Audit", "uSeoToolkit_SiteAudit_Migration")
+        { }
+
+        protected override void DefinePlan()
+        {
+            To<SiteAuditInitialMigration>("state-1");
+        }
+    }
+}
