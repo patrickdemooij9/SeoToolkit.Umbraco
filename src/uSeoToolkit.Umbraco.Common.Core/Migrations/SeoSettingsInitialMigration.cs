fdbaee90ac858b732b9a3278ca7541915e582cc4
leekelleher <leekelleher@gmail.com>
2022-02-27 15:17:22 +0000

    Changes Migration to PackageMigration
    Following @KevinJump's post, this will also add the package to the
    listing in the backoffice.
    https://dev.to/kevinjump/put-your-package-in-the-installed-package-list-in-umbraco-9-11cg
    
    This also removes the separate Component, as Umbraco
    auto-detects the package migration.

--- src/uSeoToolkit.Umbraco.Common.Core/Migrations/SeoSettingsInitialMigration.cs
+++ src/uSeoToolkit.Umbraco.Common.Core/Migrations/SeoSettingsInitialMigration.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using Umbraco.Cms.Infrastructure.Migrations;
+﻿using Umbraco.Cms.Infrastructure.Migrations;
 using uSeoToolkit.Umbraco.Common.Core.Models.Database;
 
 namespace uSeoToolkit.Umbraco.Common.Core.Migrations
 {
     public class SeoSettingsInitialMigration : MigrationBase
     {
-        public SeoSettingsInitialMigration(IMigrationContext context) : base(context)
-        {
-        }
+        public SeoSettingsInitialMigration(IMigrationContext context)
+            : base(context)
+        { }
 
         protected override void Migrate()
         {
