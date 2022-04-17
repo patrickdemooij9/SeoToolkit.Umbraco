9f38c98b252a8962c4691e85ce9d943b7fa05c0a
patrickdemooij9 <patrickdemooij98@hotmail.com>
2022-04-17 16:41:27 +0200

    Support for multiple media picker (#66)

---
+++ src/SeoToolkit.Umbraco.MetaFields.Core/Common/Converters/SeoValueConverters/MultiplePublishedContentSeoValueConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SeoToolkit.Umbraco.MetaFields.Core.Interfaces.Converters;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Extensions;
+
+namespace SeoToolkit.Umbraco.MetaFields.Core.Common.Converters.SeoValueConverters
+{
+    public class MultiplePublishedContentSeoValueConverter : ISeoValueConverter
+    {
+        public Type FromValue => typeof(IEnumerable<IPublishedContent>);
+        public Type ToValue => typeof(string);
+        public object Convert(object value, IPublishedContent currentContent, string fieldAlias)
+        {
+            if (value is not IEnumerable<IPublishedContent> castedValue)
+                return null;
+
+            return castedValue.FirstOrDefault()?.Url(mode: UrlMode.Absolute);
+        }
+    }
+}
