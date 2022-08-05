a3352b6e805f6cf6a171ff41098cbba3b9a149a3
Patrick de Mooij <patrickdemooij98@hotmail.com>
2022-08-05 11:36:26 +0200

    Common package & main package static assets

---
+++ src/SeoToolkit.Umbraco.Common/ManifestLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Manifest;
+
+namespace SeoToolkit.Umbraco.Common
+{
+    internal class ManifestLoader : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder.ManifestFilters().Append<ManifestFilter>();
+        }
+    }
+
+    internal class ManifestFilter : IManifestFilter
+    {
+        public void Filter(List<PackageManifest> manifests)
+        {
+            manifests.Add(new PackageManifest
+            {
+                PackageName = "SeoToolkit.Umbraco.Common",
+                Version = "2.0.0",
+                Scripts = new[]
+                {
+                    "/App_Plugins/SeoToolkit/Dashboards/welcomeDashboard.controller.js",
+                    "/App_Plugins/SeoToolkit/ContentApps/DocumentType/seoSettings.controller.js",
+                    "/App_Plugins/SeoToolkit/ContentApps/Content/seoContent.controller.js"
+                },
+                Stylesheets = new[]
+                {
+                    "/App_Plugins/SeoToolkit/css/main.css"
+                }
+            });
+        }
+    }
+}
---
+++ src/SeoToolkit.Umbraco/ManifestLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Manifest;
+
+namespace SeoToolkit.Umbraco
+{
+    internal class ManifestLoader : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder.ManifestFilters().Append<ManifestFilter>();
+        }
+    }
+
+    internal class ManifestFilter : IManifestFilter
+    {
+        public void Filter(List<PackageManifest> manifests)
+        {
+            manifests.Add(new PackageManifest
+            {
+                PackageName = "SeoToolkit.Umbraco",
+                Version = "2.0.0",
+                Scripts = new[]
+                {
+                    "/App_Plugins/SeoToolkit/backoffice/License/licenseDashboard.controller.js"
+                }
+            });
+        }
+    }
+}
