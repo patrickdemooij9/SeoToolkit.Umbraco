2331d54d079829e3caf7a779b72e4cc70588e10d
patrickdemooij9 <patrickdemooij98@hotmail.com>
2022-08-05 11:38:05 +0200

    Static assets (#102)
    * Static assets for MetaFields
    
    * Setup static assets for redirects
    
    * Robots.txt static assets
    
    * ScriptManager static assets
    
    * SiteAudit static assets
    
    * Sitemap static assets
    
    * Common package & main package static assets

---
+++ src/SeoToolkit.Umbraco.Sitemap/ManifestLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Manifest;
+
+namespace SeoToolkit.Umbraco.Sitemap
+{
+    internal class ManifestLoader : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder.ManifestFilters().Append<ManifestFilter>();
+        }
+    }
+
+    internal class ManifestFilter : IManifestFilter
+    {
+        public void Filter(List<PackageManifest> manifests)
+        {
+            manifests.Add(new PackageManifest
+            {
+                PackageName = "SeoToolkit.Umbraco.Sitemap",
+                Version = "2.0.0",
+                Scripts = new[]
+                {
+                    "/App_Plugins/SeoToolkit/Sitemap/Displays/DocumentType/sitemapSettings.controller.js"
+                }
+            });
+        }
+    }
+}
