2cf8061b1f8b1bd65d04fa723dc1ad81338262ab
Patrick de Mooij <patrickdemooij98@hotmail.com>
2022-08-05 11:21:20 +0200

    Robots.txt static assets

---
+++ src/SeoToolkit.Umbraco.RobotsTxt/ManifestLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Manifest;
+
+namespace SeoToolkit.Umbraco.RobotsTxt
+{
+    internal class ManifestLoader : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder.ManifestFilters().Append<ManifestFilter>();
+        }
+    }
+
+    internal class ManifestFilter : IManifestFilter
+    {
+        public void Filter(List<PackageManifest> manifests)
+        {
+            manifests.Add(new PackageManifest
+            {
+                PackageName = "SeoToolkit.Umbraco.RobotsTxt",
+                Version = "2.0.0",
+                Scripts = new[]
+                {
+                    "/App_Plugins/SeoToolkit/backoffice/RobotsTxt/detail.controller.js"
+                }
+            });
+        }
+    }
+}
