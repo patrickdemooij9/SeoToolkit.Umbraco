da8af03bed13862bdd83d07b0b067c96e814ba8c
Patrick de Mooij <patrickdemooij98@hotmail.com>
2022-10-26 21:47:39 +0200

    Allow for custom open graph crop

--- src/SeoToolkit.Umbraco.MetaFields.Core/Common/Converters/SeoValueConverters/PublishedContentSeoValueConverter.cs
+++ src/SeoToolkit.Umbraco.MetaFields.Core/Common/Converters/SeoValueConverters/PublishedContentSeoValueConverter.cs
@@ -2,6 +2,10 @@
 using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Extensions;
 using SeoToolkit.Umbraco.MetaFields.Core.Interfaces.Converters;
+using System.IO;
+using SeoToolkit.Umbraco.Common.Core.Services.SettingsService;
+using SeoToolkit.Umbraco.MetaFields.Core.Config.Models;
+using System.Linq;
 
 namespace SeoToolkit.Umbraco.MetaFields.Core.Common.Converters.SeoValueConverters
 {
@@ -9,9 +13,27 @@ namespace SeoToolkit.Umbraco.MetaFields.Core.Common.Converters.SeoValueConverter
     {
         public Type FromValue => typeof(IPublishedContent);
         public Type ToValue => typeof(string);
+
+        private ISettingsService<MetaFieldsConfigModel> _settingsService;
+
+        public PublishedContentSeoValueConverter(ISettingsService<MetaFieldsConfigModel> settingsService)
+        {
+            _settingsService = settingsService;
+        }
+
         public object Convert(object value, IPublishedContent currentContent, string fieldAlias)
         {
-            return (value as IPublishedContent)?.Url(mode: UrlMode.Absolute);
+            if (value is not IPublishedContent content) return null;
+
+            var settings = _settingsService.GetSettings();
+            if (content.ItemType == PublishedItemType.Media)
+            {
+                if (!settings.SupportedMediaTypes.Contains(Path.GetExtension(currentContent.Url()), StringComparer.InvariantCultureIgnoreCase)) return null;
+
+                return string.IsNullOrWhiteSpace(settings.OpenGraphCropAlias) ? currentContent.GetCropUrl(urlMode: UrlMode.Absolute) : currentContent.GetCropUrl(settings.OpenGraphCropAlias, UrlMode.Absolute);
+            }
+
+            return content.Url(mode: UrlMode.Absolute);
         }
     }
 }
--- src/SeoToolkit.Umbraco.MetaFields.Core/Config/Models/MetaFieldsConfigModel.cs
+++ src/SeoToolkit.Umbraco.MetaFields.Core/Config/Models/MetaFieldsConfigModel.cs
@@ -4,6 +4,8 @@ namespace SeoToolkit.Umbraco.MetaFields.Core.Config.Models
 {
     public class MetaFieldsConfigModel
     {
+        public string[] SupportedMediaTypes { get; set; } = Array.Empty<string>();
+        public string OpenGraphCropAlias { get; set; }
         public string[] DisabledModules { get; set; } = Array.Empty<string>();
     }
 }
