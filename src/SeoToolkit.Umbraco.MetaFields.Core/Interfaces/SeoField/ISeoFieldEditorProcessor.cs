2dec3babe8fb96edaa51eaa1091129e5f7177c57
Patrick de Mooij <patrickdemooij98@hotmail.com>
2023-01-17 19:50:27 +0100

    Don't allow invalid data for robot value

---
+++ src/SeoToolkit.Umbraco.MetaFields.Core/Interfaces/SeoField/ISeoFieldEditorProcessor.cs
@@ -0,0 +1,11 @@
+﻿namespace SeoToolkit.Umbraco.MetaFields.Core.Interfaces.SeoField
+{
+    public interface ISeoFieldEditorProcessor
+    {
+        /// <summary>
+        /// Handle the value before sending it through the converter
+        /// </summary>
+        /// <param name="value"></param>
+        object HandleValue(object value);
+    }
+}
