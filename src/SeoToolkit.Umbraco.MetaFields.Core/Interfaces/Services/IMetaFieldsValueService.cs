e2d49113dc8356af9cf7331fa6f385346be914d9
patrickdemooij9 <patrickdemooij98@hotmail.com>
2023-03-06 17:38:05 +0100

    Make sure to set the variationContext for sitemap (#166)
    * Make sure to set the variationContext for sitemap
    
    * Some fixes
    
    * Last fix

--- src/SeoToolkit.Umbraco.MetaFields.Core/Interfaces/Services/IMetaFieldsValueService.cs
+++ src/SeoToolkit.Umbraco.MetaFields.Core/Interfaces/Services/IMetaFieldsValueService.cs
@@ -6,8 +6,21 @@ namespace SeoToolkit.Umbraco.MetaFields.Core.Interfaces.Services
 {
     public interface IMetaFieldsValueService
     {
-        Dictionary<string, object> GetUserValues(int nodeId);
-        void AddValues(int nodeId, Dictionary<string, object> values);
-        void Delete(int nodeId, string fieldAlias);
+        /// <summary>
+        /// Get the values set by the user. If culture is NULL, the variation context culture will be used.
+        /// </summary>
+        /// <param name="nodeId"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        Dictionary<string, object> GetUserValues(int nodeId, string culture = null);
+
+        /// <summary>
+        /// Set the values by the user. If culture is NULL, the variation context culture will be used.
+        /// </summary>
+        /// <param name="nodeId"></param>
+        /// <param name="values"></param>
+        /// <param name="culture"></param>
+        void AddValues(int nodeId, Dictionary<string, object> values, string culture = null);
+        void Delete(int nodeId, string fieldAlias, string culture = null);
     }
 }
