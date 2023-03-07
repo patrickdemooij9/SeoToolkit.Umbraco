a2b8f2f3811af89b9986001e4759f1907f0bc590
Patrick de Mooij <patrickdemooij98@hotmail.com>
2023-03-07 19:13:22 +0100

    Updated versions

--- src/SeoToolkit.Umbraco.RobotsTxt/ManifestLoader.cs
+++ src/SeoToolkit.Umbraco.RobotsTxt/ManifestLoader.cs
@@ -20,7 +20,7 @@ namespace SeoToolkit.Umbraco.RobotsTxt
             manifests.Add(new PackageManifest
             {
                 PackageName = "SeoToolkit.Umbraco.RobotsTxt",
-                Version = "2.3.0",
+                Version = "2.3.1",
                 Scripts = new[]
                 {
                     "/App_Plugins/SeoToolkit/backoffice/RobotsTxt/detail.controller.js"
--- src/SeoToolkit.Umbraco/ManifestLoader.cs
+++ src/SeoToolkit.Umbraco/ManifestLoader.cs
@@ -20,7 +20,7 @@ namespace SeoToolkit.Umbraco
             manifests.Add(new PackageManifest
             {
                 PackageName = "SeoToolkit.Umbraco",
-                Version = "2.3.0",
+                Version = "2.3.1",
                 Scripts = new[]
                 {
                     "/App_Plugins/SeoToolkit/backoffice/License/licenseDashboard.controller.js"
