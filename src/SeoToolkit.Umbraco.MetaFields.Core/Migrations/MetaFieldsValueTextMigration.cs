447b3bf549e25855254bb6d17d28c5b17842ba01
patrickdemooij9 <patrickdemooij98@hotmail.com>
2023-04-27 10:24:17 +0200

    Set UserValue column to nvarchar(max) (#181)

---
+++ src/SeoToolkit.Umbraco.MetaFields.Core/Migrations/MetaFieldsValueTextMigration.cs
@@ -0,0 +1,16 @@
+﻿using Umbraco.Cms.Infrastructure.Migrations;
+
+namespace SeoToolkit.Umbraco.MetaFields.Core.Migrations
+{
+    public class MetaFieldsValueTextMigration : MigrationBase
+    {
+        public MetaFieldsValueTextMigration(IMigrationContext context) : base(context)
+        {
+        }
+
+        protected override void Migrate()
+        {
+            Database.Execute($"ALTER TABLE SeoToolkitMetaFieldsValue ALTER COLUMN UserValue nvarchar(max);");
+        }
+    }
+}
