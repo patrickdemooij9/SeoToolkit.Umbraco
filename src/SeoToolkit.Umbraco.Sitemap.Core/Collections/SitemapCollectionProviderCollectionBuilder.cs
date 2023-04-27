1e40cb041807f0632457b45a7f5cf5bc74b3e3a3
patrickdemooij9 <patrickdemooij98@hotmail.com>
2023-04-27 14:05:46 +0200

    Add logic for sitemap collections

---
+++ src/SeoToolkit.Umbraco.Sitemap.Core/Collections/SitemapCollectionProviderCollectionBuilder.cs
@@ -0,0 +1,10 @@
+﻿using SeoToolkit.Umbraco.Sitemap.Core.Interfaces;
+using Umbraco.Cms.Core.Composing;
+
+namespace SeoToolkit.Umbraco.Sitemap.Core.Collections
+{
+    public class SitemapCollectionProviderCollectionBuilder : OrderedCollectionBuilderBase<SitemapCollectionProviderCollectionBuilder, SitemapCollectionProviderCollection, ISitemapCollectionProvider>
+    {
+        protected override SitemapCollectionProviderCollectionBuilder This => this;
+    }
+}
---
+++ src/SeoToolkit.Umbraco.Sitemap.Core/Collections/SitmapCollectionProviderExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Umbraco.Cms.Core.DependencyInjection;
+
+namespace SeoToolkit.Umbraco.Sitemap.Core.Collections
+{
+    public static class SitmapCollectionProviderExtensions
+    {
+        public static SitemapCollectionProviderCollectionBuilder SitemapCollections(this IUmbracoBuilder builder)
+            => builder.WithCollectionBuilder<SitemapCollectionProviderCollectionBuilder>();
+    }
+}
