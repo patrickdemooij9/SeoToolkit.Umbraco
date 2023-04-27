fac4bffc44a7faa0b17fb62c062362293a1bf17c
Patrick de Mooij <patrickdemooij98@hotmail.com>
2023-04-27 11:12:06 +0200

    Update version

--- src/SeoToolkit.Umbraco.MetaFields/ManifestLoader.cs
+++ src/SeoToolkit.Umbraco.MetaFields/ManifestLoader.cs
@@ -20,7 +20,7 @@ namespace SeoToolkit.Umbraco.MetaFields
             manifests.Add(new PackageManifest
             {
                 PackageName = "SeoToolkit.Umbraco.MetaFields",
-                Version = "3.1.2",
+                Version = "3.1.3",
                 Scripts = new[]
                 {
                     "/App_Plugins/SeoToolkit/MetaFields/Interface/ContentApps/DocumentSettings/documentSettings.controller.js",
--- src/SeoToolkit.Umbraco/ManifestLoader.cs
+++ src/SeoToolkit.Umbraco/ManifestLoader.cs
@@ -20,7 +20,7 @@ namespace SeoToolkit.Umbraco
             manifests.Add(new PackageManifest
             {
                 PackageName = "SeoToolkit.Umbraco",
-                Version = "3.1.2",
+                Version = "3.1.3",
                 Scripts = new[]
                 {
                     "/App_Plugins/SeoToolkit/backoffice/License/licenseDashboard.controller.js"
