1e518ba08686e2fde470a41cf658e5d9a45b7f85
patrickdemooij9 <patrickdemooij98@hotmail.com>
2023-05-10 22:19:02 +0200

    185: Fix migration for SQLite

--- src/SeoToolkit.Umbraco.MetaFields.Core/Migrations/MetaFieldsValueTextMigration.cs
+++ src/SeoToolkit.Umbraco.MetaFields.Core/Migrations/MetaFieldsValueTextMigration.cs
@@ -1,4 +1,6 @@
-﻿using Umbraco.Cms.Infrastructure.Migrations;
+﻿using SeoToolkit.Umbraco.MetaFields.Core.Models.SeoSettings.Database;
+using Umbraco.Cms.Infrastructure.Migrations;
+using Umbraco.Extensions;
 
 namespace SeoToolkit.Umbraco.MetaFields.Core.Migrations
 {
@@ -10,7 +12,19 @@ namespace SeoToolkit.Umbraco.MetaFields.Core.Migrations
 
         protected override void Migrate()
         {
-            Database.Execute($"ALTER TABLE SeoToolkitMetaFieldsValue ALTER COLUMN UserValue nvarchar(max);");
+            if (DatabaseType == NPoco.DatabaseType.SQLite)
+            {
+                //SQLite doesn't support normal altering of columns. https://github.com/umbraco/Umbraco-CMS/issues/12676
+                Database.Execute("ALTER TABLE SeoToolkitMetaFieldsValue RENAME TO old_SeoToolkitMetaFieldsValue;");
+                Create.Table<MetaFieldsValueEntity>().Do();
+                Database.InsertBulk(Database.Fetch<MetaFieldsValueEntity>(Sql()
+                    .SelectAll()
+                    .From("old_SeoToolkitMetaFieldsValue")));
+            }
+            else
+            {
+                Database.Execute($"ALTER TABLE SeoToolkitMetaFieldsValue ALTER COLUMN UserValue nvarchar(max);");
+            }
         }
     }
 }
