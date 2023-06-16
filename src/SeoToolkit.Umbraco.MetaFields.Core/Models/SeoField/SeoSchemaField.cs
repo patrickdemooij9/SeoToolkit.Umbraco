4960afcd51baafe3f4b05a111eb3072ace78b68f
Uğurhan Gül <ugurhan.gul@clockwork.com.tr>
2023-06-16 16:45:47 +0300

    Update SeoSchemaField.cs

--- src/SeoToolkit.Umbraco.MetaFields.Core/Models/SeoField/SeoSchemaField.cs
+++ src/SeoToolkit.Umbraco.MetaFields.Core/Models/SeoField/SeoSchemaField.cs
@@ -22,7 +22,7 @@ namespace SeoToolkit.Umbraco.MetaFields.Core.Models.SeoField
 
         public HtmlString Render(object value)
         {
-            return new HtmlString($"<script type=\"application/ld+json\" />{value}</script>");
+            return new HtmlString($"{value}");
         }
     }
 }
