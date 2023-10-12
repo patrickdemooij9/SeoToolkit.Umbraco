d09dd547673d448f11777c229710f3c46d075914
patrickdemooij9 <patrickdemooij98@hotmail.com>
2023-10-12 18:50:52 +0200

    Added structure mode for Sitemap generation (#246)

---
+++ src/SeoToolkit.Umbraco.Sitemap.Core/Config/StructureMode.cs
@@ -0,0 +1,8 @@
+﻿namespace SeoToolkit.Umbraco.Sitemap.Core.Config
+{
+    public enum StructureMode
+    {
+        MultiRoot, // Multiple homepages/websites on the root. Default
+        OnlyRoot, // Whole website spreaded out around the root. No way for extra websites.
+    }
+}
