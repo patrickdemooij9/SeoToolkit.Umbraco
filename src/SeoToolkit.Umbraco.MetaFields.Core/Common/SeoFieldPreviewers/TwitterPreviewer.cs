aba379f61b8a8a701227cf2f65a1d15e2af3f657
Uğurhan Gül <ugurhan.gul@clockwork.com.tr>
2024-08-22 15:19:36 +0300

    opengraph and twitter groups previews

---
+++ src/SeoToolkit.Umbraco.MetaFields.Core/Common/SeoFieldPreviewers/TwitterPreviewer.cs
@@ -0,0 +1,11 @@
+﻿using SeoToolkit.Umbraco.MetaFields.Core.Constants;
+using SeoToolkit.Umbraco.MetaFields.Core.Interfaces.SeoField;
+
+namespace SeoToolkit.Umbraco.MetaFields.Core.Common.SeoFieldPreviewers
+{
+    public class TwitterPreviewer : ISeoFieldPreviewer
+    {
+        public string Group => SeoFieldGroupConstants.TwitterGroup;
+        public string View => "/App_Plugins/SeoToolkit/MetaFields/Interface/Previewers/OpenGraph/openGraphPreviewer.html";
+    }
+}
