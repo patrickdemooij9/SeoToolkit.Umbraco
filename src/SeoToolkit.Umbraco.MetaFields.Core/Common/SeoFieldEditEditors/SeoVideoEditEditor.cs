dfab4926db7f5a0725d87904f0b4cb1ab9a0e518
Uğurhan Gül <ugurhan.gul@clockwork.com.tr>
2024-08-22 14:41:10 +0300

    SeoVideoEditEditor media picker property

---
+++ src/SeoToolkit.Umbraco.MetaFields.Core/Common/SeoFieldEditEditors/SeoVideoEditEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Umbraco.Cms.Core.Web;
+using SeoToolkit.Umbraco.MetaFields.Core.Common.Converters.EditorConverters;
+using SeoToolkit.Umbraco.MetaFields.Core.Interfaces.Converters;
+using SeoToolkit.Umbraco.MetaFields.Core.Interfaces.SeoField;
+
+namespace SeoToolkit.Umbraco.MetaFields.Core.Common.SeoFieldEditEditors
+{
+    public class SeoVideoEditEditor : ISeoFieldEditEditor
+    {
+        public string View => "mediapicker";
+        public Dictionary<string, object> Config { get; }
+        public IEditorValueConverter ValueConverter { get; }
+
+        public SeoVideoEditEditor(IUmbracoContextFactory umbracoContextFactory)
+        {
+            ValueConverter = new UmbracoMediaUdiConverter(umbracoContextFactory);
+            Config = new Dictionary<string, object>
+            {
+                {"disableFolderSelect", false},
+                {"idType", "udi"},
+                {"ignoreUserStartNodes", false},
+                {"multiPicker", false},
+                {"onlyImages", false}
+            };
+        }
+    }
+}
