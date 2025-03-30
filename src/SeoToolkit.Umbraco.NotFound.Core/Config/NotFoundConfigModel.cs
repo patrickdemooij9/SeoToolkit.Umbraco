4d031220677af6f34e4c16aea425ab9f101166f2
patrickdemooij9 <11466511+patrickdemooij9@users.noreply.github.com>
2025-03-30 13:25:20 +0200

    Add way to disable component

---
+++ src/SeoToolkit.Umbraco.NotFound.Core/Config/NotFoundConfigModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SeoToolkit.Umbraco.NotFound.Core.Config
+{
+    public class NotFoundConfigModel
+    {
+        public string[] DisabledModules { get; set; } = Array.Empty<string>();
+    }
+}
