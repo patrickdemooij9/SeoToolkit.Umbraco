8e8b39ad9b9e4c0c05e924d2f845fab2b055057f
patrickdemooij9 <11466511+patrickdemooij9@users.noreply.github.com>
2025-04-08 20:36:46 +0200

    Update version

--- src/SeoToolkit.Umbraco.NotFound/ManifestLoader.cs
+++ src/SeoToolkit.Umbraco.NotFound/ManifestLoader.cs
@@ -33,7 +33,7 @@ namespace SeoToolkit.Umbraco.NotFound
                 Id = "SeoToolkit.Umbraco.NotFound",
                 Name = "SeoToolkit NotFound",
                 AllowTelemetry = true,
-                Version = "4.0.0-beta5",
+                Version = "4.0.0-beta6",
                 Extensions = [ entrypoint!],
             }
             ];
--- src/SeoToolkit.Umbraco.Redirects/ManifestLoader.cs
+++ src/SeoToolkit.Umbraco.Redirects/ManifestLoader.cs
@@ -33,7 +33,7 @@ namespace SeoToolkit.Umbraco.Redirects
                 Id = "SeoToolkit.Umbraco.Redirects",
                 Name = "SeoToolkit Redirects",
                 AllowTelemetry = true,
-                Version = "4.0.0-beta5",
+                Version = "4.0.0-beta6",
                 Extensions = [ entrypoint!],
             }
             ];
