cfdacbe810613486f45b6ed66fff9c541c76f355
patrickdemooij9 <11466511+patrickdemooij9@users.noreply.github.com>
2025-06-20 15:28:27 +0200

    Fix some issues with Bloomfilter on installing

--- src/SeoToolkit.Umbraco.Redirects.Core/BackgroundTasks/RebuildBloomFilterTask.cs
+++ src/SeoToolkit.Umbraco.Redirects.Core/BackgroundTasks/RebuildBloomFilterTask.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Infrastructure.HostedServices;
 
@@ -13,13 +14,18 @@ namespace SeoToolkit.Umbraco.Redirects.Core.BackgroundTasks
     internal class RebuildBloomFilterTask : RecurringHostedServiceBase
     {
         private readonly IRedirectsBloomFilter _redirectsBloomFilter;
+        private readonly IRuntimeState _runtimeState;
 
-        public RebuildBloomFilterTask(IRuntimeState runtimeState, ILogger<RebuildBloomFilterTask> logger, IRedirectsBloomFilter redirectsBloomFilter) : base(logger, TimeSpan.FromMinutes(5), TimeSpan.FromMinutes(5))
+        public RebuildBloomFilterTask(IRuntimeState runtimeState, ILogger<RebuildBloomFilterTask> logger, IRedirectsBloomFilter redirectsBloomFilter) : base(logger, TimeSpan.FromMinutes(5), TimeSpan.FromMinutes(0))
         {
             _redirectsBloomFilter = redirectsBloomFilter;
+            _runtimeState = runtimeState;
         }
         public override Task PerformExecuteAsync(object state)
         {
+            if (_runtimeState.Level != RuntimeLevel.Run)
+                return Task.CompletedTask;
+
             if (_redirectsBloomFilter.ShouldRebuild)
             {
                 _redirectsBloomFilter.Rebuild();
