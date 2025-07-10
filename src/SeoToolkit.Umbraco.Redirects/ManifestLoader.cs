6f058790d2eb346c81ff868bb243dd5028d62c60
patrickdemooij9 <11466511+patrickdemooij9@users.noreply.github.com>
2025-07-10 20:17:55 +0200

    Up versions

--- src/SeoToolkit.Umbraco.Redirects/ManifestLoader.cs
+++ src/SeoToolkit.Umbraco.Redirects/ManifestLoader.cs
@@ -33,7 +33,7 @@ namespace SeoToolkit.Umbraco.Redirects
                 Id = "SeoToolkit.Umbraco.Redirects",
                 Name = "SeoToolkit Redirects",
                 AllowTelemetry = true,
-                Version = "4.1.0",
+                Version = "4.1.2",
                 Extensions = [ entrypoint!],
             }
             ];
