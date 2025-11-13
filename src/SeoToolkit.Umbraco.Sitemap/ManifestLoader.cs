436e570f2d7c3775569964b5debf0b6d8247c8ab
patrickdemooij9 <11466511+patrickdemooij9@users.noreply.github.com>
2025-11-13 08:34:45 +0100

    Up version

--- src/SeoToolkit.Umbraco.Sitemap/ManifestLoader.cs
+++ src/SeoToolkit.Umbraco.Sitemap/ManifestLoader.cs
@@ -33,7 +33,7 @@ namespace SeoToolkit.Umbraco.Sitemap
                 Id = "SeoToolkit.Umbraco.Sitemap",
                 Name = "SeoToolkit Sitemap",
                 AllowTelemetry = true,
-                Version = "5.1.0",
+                Version = "5.1.1",
                 Extensions = [ entrypoint!],
             }
             ];
