2334fef7b650946df83b63939f2b6e3319625e2e
patrickdemooij9 <11466511+patrickdemooij9@users.noreply.github.com>
2025-12-21 21:24:21 +0100

    Up version

--- src/SeoToolkit.Umbraco.Common/ManifestLoader.cs
+++ src/SeoToolkit.Umbraco.Common/ManifestLoader.cs
@@ -34,7 +34,7 @@ namespace SeoToolkit.Umbraco.Common
                 Id = "SeoToolkit.Umbraco.Common",
                 Name = "SeoToolkit Common",
                 AllowTelemetry = true,
-                Version = "6.0.1",
+                Version = "6.0.2",
                 Extensions = [ entrypoint!],
             }
             ];
--- src/SeoToolkit.Umbraco.NotFound/ManifestLoader.cs
+++ src/SeoToolkit.Umbraco.NotFound/ManifestLoader.cs
@@ -33,7 +33,7 @@ namespace SeoToolkit.Umbraco.NotFound
                 Id = "SeoToolkit.Umbraco.NotFound",
                 Name = "SeoToolkit NotFound",
                 AllowTelemetry = true,
-                Version = "4.0.0",
+                Version = "6.0.2",
                 Extensions = [ entrypoint!],
             }
             ];
