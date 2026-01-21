76e70e5796771c37aa3b55258b66327d75f9ea7c
patrickdemooij9 <11466511+patrickdemooij9@users.noreply.github.com>
2026-01-21 20:28:15 +0100

    Move page not found to settings

---
+++ src/SeoToolkit.Umbraco.NotFound.Core/Settings/NotFoundPageSeoSetting.cs
@@ -0,0 +1,18 @@
+﻿using SeoToolkit.Umbraco.Common.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeoToolkit.Umbraco.NotFound.Core.Settings
+{
+    public class NotFoundPageSeoSetting : ISeoKeyValueSetting
+    {
+        public string Key => NotFoundConstants.NotFoundKeyValueKey;
+
+        public string Title => "Page not found";
+
+        public string Description => "Select your 404 page here.";
+
+        public string PropertyAlias => "Umb.PropertyEditorUi.DocumentPicker";
+    }
+}
