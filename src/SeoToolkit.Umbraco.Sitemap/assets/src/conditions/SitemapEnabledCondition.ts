7308242fcfe48de9139647acb54d4e7817a36d01
patrickdemooij9 <patrickdemooij98@hotmail.com>
2026-04-23 13:46:43 +0200

    Fix issue with loading sitemap data even if it is disabled (#515)
    Co-authored-by: patrickdemooij9 <11466511+patrickdemooij9@users.noreply.github.com>

--- src/SeoToolkit.Umbraco.Sitemap/assets/src/conditions/SitemapEnabledCondition.ts
+++ src/SeoToolkit.Umbraco.Sitemap/assets/src/conditions/SitemapEnabledCondition.ts
@@ -8,30 +8,36 @@ import { UmbControllerHost } from "@umbraco-cms/backoffice/controller-api";
 import PageSettingsRepository from "../repositories/pageSettingsRepository";
 import { UMB_DOCUMENT_WORKSPACE_CONTEXT } from "@umbraco-cms/backoffice/document";
 
-export type SitemapEnabledConditionConfig = UmbConditionConfigBase
+export type SitemapEnabledConditionConfig = UmbConditionConfigBase;
 
 export class SitemapEnabledCondition
   extends UmbConditionBase<SitemapEnabledConditionConfig>
   implements UmbExtensionCondition
 {
-  #repository: PageSettingsRepository = new PageSettingsRepository(
-    this
-  );
+  #repository: PageSettingsRepository = new PageSettingsRepository(this);
 
   constructor(
     host: UmbControllerHost,
-    args: UmbConditionControllerArguments<SitemapEnabledConditionConfig>
+    args: UmbConditionControllerArguments<SitemapEnabledConditionConfig>,
   ) {
     super(host, args);
 
     this.consumeContext(UMB_DOCUMENT_WORKSPACE_CONTEXT, (context) => {
-      this.observe(context?.contentTypeUnique, (value) => {
-        if (!value) {
+      this.#repository.isModuleEnabled().then((resp) => {
+        this.permitted = resp.data ?? false;
+
+        if (!this.permitted) {
           return;
         }
 
-        this.#repository.getPageSettings(value).then((resp) => {
-          this.permitted = !resp.data.hideFromSitemap;
+        this.observe(context?.contentTypeUnique, (value) => {
+          if (!value) {
+            return;
+          }
+
+          this.#repository.getPageSettings(value).then((resp) => {
+            this.permitted = !resp.data.hideFromSitemap;
+          });
         });
       });
     });
