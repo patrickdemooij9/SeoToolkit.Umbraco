da7a13bca44c4f492561792f114a451a5ba19ef0
patrickdemooij9 <patrickdemooij98@hotmail.com>
2026-05-02 16:42:38 +0200

    Add seo fields to delivery api output (#517)
    Co-authored-by: patrickdemooij9 <11466511+patrickdemooij9@users.noreply.github.com>

---
+++ src/SeoToolkit.Umbraco.Common.Core/DeliveryApi/SeoToolkitContentResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Umbraco.Cms.Core.Models.DeliveryApi;
+
+namespace SeoToolkit.Umbraco.Common.Core.DeliveryApi
+{
+    public class SeoToolkitContentResponse : ApiContentResponse
+    {
+        public SeoToolkitContentResponse(Guid id, string name, string contentType, DateTime createDate, DateTime updateDate, IApiContentRoute route, IDictionary<string, object?> properties, IDictionary<string, IApiContentRoute> cultures, Dictionary<string, object> seoToolkit) : base(id, name, contentType, createDate, updateDate, route, properties, cultures)
+        {
+            SeoToolkit = seoToolkit;
+        }
+
+        public Dictionary<string, object> SeoToolkit { get; set; } = new();
+    }
+}
