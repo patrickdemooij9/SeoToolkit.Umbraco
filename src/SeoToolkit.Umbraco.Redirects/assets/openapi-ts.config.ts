65c1808e4cbdb3631d95fc15e04b7efc22f3f1d2
patrickdemooij9 <patrickdemooij98@hotmail.com>
2026-08-24 10:57:33 +0200

    Improve the generation of the different packages (#569)
    Co-authored-by: patrickdemooij9 <11466511+patrickdemooij9@users.noreply.github.com>

--- src/SeoToolkit.Umbraco.Redirects/assets/openapi-ts.config.ts
+++ src/SeoToolkit.Umbraco.Redirects/assets/openapi-ts.config.ts
@@ -1,26 +1,3 @@
-import { defaultPlugins, defineConfig } from '@hey-api/openapi-ts';
+import { apiTags, defineModuleConfig } from '../../openapi-ts.shared';
 
-export default defineConfig({
-	input: 'http://localhost:57441/umbraco/openapi/seoToolkit.json',
-	output: {
-		path: 'src/api',
-	},
-	plugins: [
-		...defaultPlugins,
-		{
-			name: '@hey-api/client-fetch',
-			exportFromIndex: true,
-			throwOnError: true,
-		},
-		{
-			name: '@hey-api/typescript',
-			enums: 'typescript',
-		},
-		{
-			name: '@hey-api/sdk',
-			operations: {
-				strategy: 'byTags'
-			}
-		},
-	],
-});
+export default defineModuleConfig(apiTags.redirects);
